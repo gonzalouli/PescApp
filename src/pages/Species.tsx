5e73c5500c39fed805e02e6d29aa19a7ace19837
GonzaloUli <gonzalo.ulibarri@gmail.com>
2022-02-07 18:57:21 +0100

    visor de pdf 1.0

--- src/pages/Species.tsx
+++ src/pages/Species.tsx
@@ -35,26 +35,29 @@ export default function Species() {
     <IonPage>
       <Fragment>
         <IonHeader className="header">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="icon icon-tabler icon-tabler-arrow-left backButton"
-            width="40"
-            height="40"
-            viewBox="0 0 24 24"
-            strokeWidth="2.5"
-            stroke="#00abfb"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            onClick={() => {
-              setIsFishSelected(false);
-            }}
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <line x1="5" y1="12" x2="19" y2="12" />
-            <line x1="5" y1="12" x2="11" y2="18" />
-            <line x1="5" y1="12" x2="11" y2="6" />
-          </svg>
+          {isFishSelected && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-arrow-left backButton"
+              width="40"
+              height="40"
+              viewBox="0 0 24 24"
+              strokeWidth="2.5"
+              stroke="#00abfb"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              onClick={() => {
+                setIsFishSelected(false);
+              }}
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <line x1="5" y1="12" x2="19" y2="12" />
+              <line x1="5" y1="12" x2="11" y2="18" />
+              <line x1="5" y1="12" x2="11" y2="6" />
+            </svg>
+          )}
+          {!isFishSelected && <BackButton refer="/my/home" />}
           <IonTitle className="tittle">Especies</IonTitle>
         </IonHeader>
         <IonContent>
