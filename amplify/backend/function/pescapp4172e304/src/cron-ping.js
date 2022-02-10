219d1b6a94cf2b6c5ba18924b5be2d5a886f6faa
GonzaloUli <gonzalo.ulibarri@gmail.com>
2022-02-10 17:57:08 +0100

    prueba en la nube dos millones

--- amplify/backend/function/pescapp4172e304/src/cron-ping.js
+++ amplify/backend/function/pescapp4172e304/src/cron-ping.js
@@ -6,14 +6,14 @@
 //   | | hour
 //   | minute
 //   second ( optional )
-const crontab = require("node-crontab");
+const cron = require("node-cron");
 const GetAllNotifications = require("./routes/services/GetAllNotifications");
 const GetTideFromPort = require("./routes/services/GetTideFromPort");
 const moment = require("moment");
 const RemoveNotificationFromDatabase = require("./routes/services/RemoveNotificationFromDatabase");
 const sendNotification = require("./routes/services/SendNotificationFirebase");
 
-const notificationSchedule = crontab.schedule("* * 48 * *", async function () {
+const notificationSchedule = cron.schedule("* * 48 * *", async function () {
   const userToNotificate = [{}];
 
   const date = moment().add(2, "day").format("YYYY-MM-DD");
