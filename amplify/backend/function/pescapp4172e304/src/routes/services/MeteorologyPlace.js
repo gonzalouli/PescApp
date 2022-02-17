d6886cb3c819f9c9770e3c3660c4fdc13f4d1351
GonzaloUli <gonzalo.ulibarri@gmail.com>
2022-02-17 15:10:36 +0100

    estableciendo el lenguaje de meteorologia en ESP

--- amplify/backend/function/pescapp4172e304/src/routes/services/MeteorologyPlace.js
+++ amplify/backend/function/pescapp4172e304/src/routes/services/MeteorologyPlace.js
@@ -4,7 +4,7 @@ const EnvironmentCredentials = require("../../EnvironmentCredentials.json");
 module.exports = async function MeteorologyPlace(lat, lng) {
   try {
     const place = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude={part}&appid=${EnvironmentCredentials.WEATHER_KEY}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&lang=es&exclude={part}&appid=${EnvironmentCredentials.WEATHER_KEY}&lang=es`
     );
     return {
       place,
