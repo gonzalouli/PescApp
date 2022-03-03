7e9cb78b7e44415e6809c3fba54569916438b917
GonzaloUli <gonzalo.ulibarri@gmail.com>
2022-03-03 21:57:59 +0100

    arreglando problemas de version beta

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,11 @@ import Amplify from "aws-amplify";
 
 import awsExports from "./aws-exports";
 import { Capacitor } from "@capacitor/core";
-if (process.env.REACT_APP_LOCAL && true) {
-  awsExports.aws_cloud_logic_custom[0].endpoint = Capacitor.isNativePlatform()
-    ? "http://10.0.2.2:4444"
-    : "http://localhost:4444";
-}
+// if (process.env.REACT_APP_LOCAL && true) {
+//   awsExports.aws_cloud_logic_custom[0].endpoint = Capacitor.isNativePlatform()
+//     ? "http://10.0.2.2:4444"
+//     : "http://localhost:4444";
+// }
 
 Amplify.configure(awsExports);
 
