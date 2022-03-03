090f4bc4280235868d0d32e207e53528cbba44c9
GonzaloUli <gonzalo.ulibarri@gmail.com>
2022-03-03 20:40:03 +0100

    pruebas de errores

--- amplify/backend/function/pescapp4172e304/src/routes/services/SendNotificationFirebase.js
+++ amplify/backend/function/pescapp4172e304/src/routes/services/SendNotificationFirebase.js
@@ -2,7 +2,14 @@ const admin = require("firebase-admin");
 const firebaseConfig = require("../../../firebase");
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
+  credential: admin.credential.cert({
+    apiKey: "AIzaSyCCK6hK8vnTL1VqBIPSZTptr1NYq4_aceE",
+    authDomain: "pesc-app-8f7f1.firebaseapp.com",
+    projectId: "pesc-app-8f7f1",
+    storageBucket: "pesc-app-8f7f1.appspot.com",
+    messagingSenderId: "937853490512",
+    appId: "1:937853490512:web:bd0f909edc03c51c3e4855",
+  }),
 });
 
 const sendNotification = async (usersToNotificate = []) => {
